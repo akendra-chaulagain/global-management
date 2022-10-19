10398ef40e3d24fb057fc969d6f5f8a4d8c33fed
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2022-10-19 17:18:59 +0545

    counter

--- resources/views/website/courses.blade.php
+++ resources/views/website/courses.blade.php
@@ -11,9 +11,7 @@
     {{ $courses->caption }}
 @endpush
 @section('content')
-
-
-  <section class="bredcrumb-area" style="background: url(/website/images/banner.jpg) no-repeat;">
+    <section class="bredcrumb-area" style="background: url(/website/images/banner.jpg) no-repeat;">
         <div class="overlay">
             <h3>{{ $courses->caption }}</h3>
         </div>
@@ -24,19 +22,20 @@
         <div class="container">
             <div class="row">
                 @foreach ($courses->courses_childs as $courses_item)
-                     <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="course-box wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">
-                        <figure><img src="{{ $courses_item->banner_image }}" alt="course"></figure>
-                        <h4>{{ $courses_item->caption }}</h4>
-                        <p>{!! $courses_item->short_content !!}</p>
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="course-box wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">
+                            @if ($courses_item->banner_image)
+                                <figure><img src="{{ $courses_item->banner_image }}" alt="course"></figure>
+                            @endif
+
+                            <h4>{{ $courses_item->caption }}</h4>
+                            <p>{!! $courses_item->short_content !!}</p>
+                        </div>
                     </div>
-                </div>
                 @endforeach
-               
-               
+
+
             </div>
         </div>
     </section>
-
-    
 @endsection
